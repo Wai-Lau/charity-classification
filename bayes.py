c9cb7e568713e9ee1030db1b36f371822f33f7d2
Wai-Lau <wl.wailau@gmail.com>
2019-04-08 14:29:35 -0400

    Convert to CauseSwipe categories

--- bayes.py
+++ bayes.py
@@ -1,3 +1,12 @@
+from pyspark.mllib.classification import NaiveBayes, NaiveBayesModel
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.linalg import SparseVector
 from cleaner import datardd as data
+
+data = data.sample(False, 0.05, seed=666)
+words = data.flatMap(lambda x: x['d'].split()).distinct().sortBy(lambda x: x).collect()
+sparselen = len(words)
+words = {k:v for v, k in enumerate(words)}
 
 import ipdb; ipdb.set_trace()
--- cleaner.py
+++ cleaner.py
@@ -34,8 +34,9 @@ polione = spark.read.csv("./data/Schedule 7_ Political Activities - Description.
                          encoding="ISO-8859-1", header=True, mode="DROPMALFORMED")
 politwo = spark.read.csv("./data/Schedule 7_ Political Activities - Outside Canada.csv",
                          encoding="ISO-8859-1", header=True, mode="DROPMALFORMED")
-cats = spark.read.csv("./data/# Categories.csv",
+cats = spark.read.csv("./data/ReducedCategories.csv",
                       encoding="ISO-8859-1", header=True, mode="DROPMALFORMED")
+catdict = cats.rdd.map(lambda x: x.asDict()).map(lambda x: (x['cat'],x['cscat'])).collectAsMap()
 
 data = newgoing.join(registered,"BN/Registration number",'full')\
         .withColumnRenamed("BN/Registration number","id")\
@@ -44,19 +45,21 @@ data = newgoing.join(registered,"BN/Registration number",'full')\
         .withColumnRenamed("Category Code", "cat")\
         .withColumnRenamed("Website Address","site")\
         .select("id","desc","active","cat","site")\
-        .sample(0.05)
+        .sample(0.05, seed=666)
 
 extradesc = polione.join(politwo.withColumnRenamed("Description","desc2"), "BN/Registration Number", 'full')\
         .withColumnRenamed("BN/Registration Number","id")\
         .withColumnRenamed("Description","desc1")\
         .select("id","desc1","desc2")
 
-datardd = data.join(extradesc, "id", "full").rdd.map(lambda x: x.asDict())\
+datardd = data.join(extradesc, "id", "left")\
+        .rdd\
+        .map(lambda x: x.asDict())\
         .map(lambda x: {
             **x,
             "d": "".join(
                 ch if unicodedata.name(ch).startswith(
-                    ('LATIN', 'DIGIT', 'SPACE', 'REPLACEMENT', 'APOSTROPHE')
+                    ('LATIN', 'DIGIT', 'SPACE', 'APOSTROPHE')
                 ) else " " \
                 for ch in " ".join(
                     [x['desc'] if x['desc'] else "",
@@ -70,8 +73,10 @@ datardd = data.join(extradesc, "id", "full").rdd.map(lambda x: x.asDict())\
             'd': " ".join(w.lower() if w.lower() not in stopwords else "" for w in x['d'].split())
         })\
         .map(lambda x: {**x, 'd': " ".join(re.sub(r'.*\d.*', '', w) for w in x['d'].split())})\
+        .map(lambda x: {**x, 'd': " ".join(re.sub(r'.*\'.*', '', w) for w in x['d'].split())})\
         .map(lambda x: {**x, 'd': re.sub(r' +',' ',x['d']).strip()})\
         .map(lambda x: {"d":x['d'], "site":x['site'], "active":x["active"], "cat":x["cat"], "id":x["id"]})\
         .filter(lambda x: x['cat'])\
-        .filter(lambda x: x['d'])
-
+        .filter(lambda x: x['d'])\
+        .map(lambda x: {**x, 'cscat': catdict[re.sub(r'^[0]+',"",x['cat'].strip())]})\
+        .map(lambda x: {"d":x['d'], "site":x['site'], "active":x["active"], "cscat":x["cscat"], "id":x["id"]})\
