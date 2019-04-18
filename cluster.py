49d36b11b9d86619f0e0538b56dfe82f25d7917f
amawai <amawai@hotmail.com>
2019-04-17 21:59:47 -0400

    add downsampling

--- cluster.py
+++ cluster.py
@@ -1,5 +1,6 @@
 import pyspark
 import random
+import numpy as np
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from cleaner import datardd as data
@@ -7,6 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer,TfidfTransformer
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
+from sklearn.utils import resample
 import matplotlib.pyplot as plt
 
 def init_spark():
@@ -22,6 +24,18 @@ def create_df(seed):
     rdd_tuples = data.map(lambda x: (x['d'], int(x['cscat'])))
     return init_spark().createDataFrame(rdd_tuples, schema)
 
+def downsample(df, seed):
+    spark = init_spark()
+    category_values = df.select('Category').distinct().collect()
+    categories = [c.Category for c in category_values]
+    category_counts = [(c, df[df.Category==c].count()) for c in categories]
+    min_count = min([x[1] for x in category_counts])
+    downsampled = []
+    for category in categories:
+        downsampled.append(resample(df[df.Category == category].collect(), replace=False, n_samples=min_count, random_state=seed))
+    downsampled = reduce((lambda x, y: x + y), downsampled)
+    return spark.createDataFrame(downsampled)
+
 def graph_by_category(pipeline, data):
     colormap = { 1: 'red', 8: 'green', 9: 'blue', 11: 'orange', 13: 'yellow', 14: 'magenta',  15: 'black'}
     texts = [a[0] for a in data]
@@ -33,7 +47,7 @@ def graph_by_category(pipeline, data):
     X = matrix.todense()
     pca = PCA(n_components=2).fit(X)
     data2D = pca.transform(X)
-    plt.scatter(data2D[:,0], data2D[:,1], c=category_colors)
+    plt.scatter(data2D[:,0], data2D[:,1], c=category_colors, s=15,linewidths=1)
     plt.show()
 
 def generate_kmeans(k, pipeline, data):
@@ -43,10 +57,11 @@ def generate_kmeans(k, pipeline, data):
     
     print("Top terms per cluster:")
     order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-    terms = pipeline.get_feature_names()
+    terms = pipeline.named_steps['vect'].get_feature_names()
     for i in range(k):
-        top_words = [terms[ind] for ind in order_centroids[i, :10]]
+        top_words = [terms[ind] for ind in order_centroids[i, :25]]
         print("Cluster {}: {}".format(i+1, ' '.join(top_words)))
+    ipdb.set_trace()
     graph_kmeans(km, matrix)
 
 def graph_kmeans(km, matrix):
@@ -60,9 +75,14 @@ def graph_kmeans(km, matrix):
         plt.scatter(centers2D[i][0], centers2D[i][1], marker=markers[i], linewidths=2, c='r')
     plt.show()
 
-df = create_df(10)
-texts = data.map(lambda x: x['d']).collect()
-pipeline = TfidfVectorizer(use_idf=False, norm='l1')
-graph_by_category(pipeline, df.rdd.collect())
-generate_kmeans(4, pipeline, texts)
 
+k = 7
+df = create_df(10)
+df = downsample(df, 10).collect()
+texts = [x['Description'] for x in df]
+pipeline = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+])
+graph_by_category(pipeline, df)
+generate_kmeans(k, pipeline, texts)
